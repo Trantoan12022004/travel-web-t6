1469e327aef5418e8de35cbf37325a2327c1e0ec
Trantoan12022004 <tranvantoan.work@gmail.com>
2025-12-19 13:38:19 +0700

    update

--- frontend/src/Apis/booking.js
+++ frontend/src/Apis/booking.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:2004/api";
+const API_URL = "https://travel-web-t6.onrender.com/api";
 
 // Tạo axios instance với config
 const bookingApi = axios.create({
